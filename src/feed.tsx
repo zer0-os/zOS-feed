524fcddd8b9386eb303cd5a422d257fe33b5049a
Hamza Khanouchi <khanouchi.hamza@gmail.com>
2022-05-09 23:20:24 +0100

    Add infinite scroll to feed items

--- src/feed.tsx
+++ src/feed.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FeedItem, Model as FeedItemModel } from './feed-item';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import './styles.css';
 
@@ -8,18 +9,62 @@ export interface Properties {
   app: string;
 }
 
-export class Feed extends React.Component<Properties> {
+interface State {
+  feed: FeedItemModel[];
+  hasMore: boolean;
+  pageNumber: number;
+  pageSize: number;
+}
+
+export class Feed extends React.Component<Properties, State> {
+  componentDidUpdate(prevProps: Properties) {
+    const { items } = this.props;
+    if (items && items !== prevProps.items) {
+      this.setState({ feed: items.slice(0, 2) });
+      this.setState({ pageNumber: 2, pageSize: 2, hasMore: true });
+    }
+  }
+
+  fetchMoreData = () => {
+    const { pageNumber, pageSize, feed } = this.state;
+    if (feed.length >= this.props.items.length) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    setTimeout(() => {
+      this.setState({
+        feed: feed.concat(
+          this.props.items.slice(
+            (pageNumber - 1) * pageSize,
+            pageNumber * pageSize
+          )
+        ),
+        pageNumber: pageNumber + 1,
+      });
+    }, 500);
+  };
+
   renderItems() {
-    const { items, app } = this.props;
-    return items.map(item => <FeedItem key={item.id} {...item} app={app} />);
+    const { app } = this.props;
+
+    return (
+      <InfiniteScroll
+        dataLength={this.state.feed.length}
+        next={this.fetchMoreData}
+        hasMore={this.state.hasMore}
+        loader={<p></p>}
+      >
+        {this.state.feed.map((item) => (
+          <FeedItem key={item.id} {...item} app={app} />
+        ))}
+      </InfiniteScroll>
+    );
   }
 
   render() {
     return (
       <div className="feed">
-        <div className="feed__items">
-          {this.renderItems()}
-        </div>
+        <div className="feed__items">{this.state && this.renderItems()}</div>
       </div>
     );
   }
