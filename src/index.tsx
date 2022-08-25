d8ef812a5520edbee7d1f339fa73776965847e3a
Joel Tulloch <joel@jtulloch.com>
2022-08-25 13:34:48 -0400

    add placeholder filters

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { AppContent, AppContextPanel, AppLayout } from '@zer0-os/zos-component-library';
 import { Provider } from 'react-redux';
 import { FeedContainer, PublicProperties } from './container';
+import { FeedFilters } from './feed-filters';
 import { store } from './store';
 
 export const App: React.FunctionComponent<PublicProperties> = (props: PublicProperties) => (
   <Provider store={store}>
     <AppLayout>
       <AppContextPanel>
+        <FeedFilters />
       </AppContextPanel>
       <AppContent>
         <FeedContainer {...props} />
