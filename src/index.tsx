52543b09eb59e0e1ecdf3714c36858d527b90492
Joel Tulloch <joel@jtulloch.com>
2022-08-25 12:56:07 -0400

    wrap feed in layout components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
+import { AppContent, AppContextPanel, AppLayout } from '@zer0-os/zos-component-library';
 import { Provider } from 'react-redux';
 import { FeedContainer, PublicProperties } from './container';
 import { store } from './store';
 
 export const App: React.FunctionComponent<PublicProperties> = (props: PublicProperties) => (
   <Provider store={store}>
-    <FeedContainer {...props} />
+    <AppLayout>
+      <AppContextPanel>
+      </AppContextPanel>
+      <AppContent>
+        <FeedContainer {...props} />
+      </AppContent>
+    </AppLayout>
   </Provider>
 );
